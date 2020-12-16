21de23b271819a0a4ca62a0d8d1a22d98a421c0b
Albert Chen <63614559+bertchen2015@users.noreply.github.com>
2020-12-15 20:26:08 -0800

    Add files via upload

---
+++ Portfolio Project/src/index.js
@@ -0,0 +1,17 @@
+const path = require( 'path' );
+const express = require( 'express' );
+const app = express();
+const publicDirectoryPath = path.join( __dirname, "../public" );
+app.use( express.static( publicDirectoryPath ) );
+app.set( 'view engine', 'hbs' );
+app.get( '/', function( req, res ) {
+  res.render( 'index', {
+    title: "Home",
+    name: "Albert"
+  } );
+} );
+const PORT = process.env.PORT || 8000;
+app.listen( PORT, () => {
+  console.log( `App listening on port ${PORT}` );
+  console.log( 'Press Ctrl+C to quit.' );
+} );
